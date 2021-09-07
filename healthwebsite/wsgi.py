77808b9ac383613c07d688e03756ab00f094dd59
Nadeemaslam <er.nadeemaslam89@gmail.com>
2021-09-07 12:18:23 +0530

    renamed website setting folder'

--- healthwebsite/wsgi.py
+++ healthwebsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for healhtwebsite project.
+WSGI config for healthwebsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healhtwebsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthwebsite.settings')
 
 application = get_wsgi_application()
