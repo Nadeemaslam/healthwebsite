84ff452266690a43a12374285e9ec98793fafd14
Nadeemaslam <er.nadeemaslam89@gmail.com>
2021-10-04 11:40:33 +0530

    all necessary changes

--- static/healthapp/js/custom.js
+++ static/healthapp/js/custom.js
@@ -70,9 +70,7 @@
 			},
 			image: {
 				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-				titleSrc: function(item) {
-					return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-				}
+
 			}
 		});
 	});
@@ -153,3 +151,35 @@
 
 	
 }(jQuery));
+
+
+	/* ..............................................
+    team popup
+    ................................................. */
+
+	$(document).ready(function() {
+		$('.popup-team').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			tLoading: 'Loading image #%curr%...',
+			mainClass: 'mfp-img-mobile',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			},
+			image: {
+				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+
+			}
+		});
+	});
+
+	/* ..............................................
+    team popup
+    ................................................. */
+
+
+
+
+
